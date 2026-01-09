17d5c04d56f015d4755e257c857a3a9adf659169
Marleen Jöckel <marleen.joeckel@gmail.com>
2026-01-09 16:56:10 +0100

    neue Icons in Map integriert

--- maptree/mapTree/map_room.gd
+++ maptree/mapTree/map_room.gd
@@ -5,14 +5,13 @@ signal selected(room: Room)
 
 const ICONS := {
 	Room.Type.NOT_ASSIGNED: [null, Vector2.ONE],
-	Room.Type.FIGHT: [preload("res://maptree/TestButtons/GakiTestButtonFight.png"), Vector2.ONE],
-	Room.Type.EVENT: [preload("res://maptree/TestButtons/GakiTestButtonEvent.png"), Vector2(0.6, 0.6)],
-	Room.Type.SHOP: [preload("res://maptree/TestButtons/GakiTestButtonShop.png"), Vector2(0.6,0.6)],
-	Room.Type.BOSS: [preload("res://maptree/TestButtons/GakiTestButtonBoss.png"), Vector2(1.25,1.25)]
+	Room.Type.FIGHT: [preload("res://maptree/MapIcons/Fight.png"), Vector2(0.13,0.13)],
+	Room.Type.EVENT: [preload("res://maptree/MapIcons/Event.png"), Vector2(0.13, 0.13)],
+	Room.Type.SHOP: [preload("res://maptree/MapIcons/Shop2.png"), Vector2(0.13,0.13)],
+	Room.Type.BOSS: [preload("res://maptree/MapIcons/Boss.png"), Vector2(0.3,0.3)]
 }
 
 @onready var sprite_2d: Sprite2D = $Visuals/Sprite2D
-@onready var line_2d: Line2D = $Visuals/Line2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var available := false : set = set_available
@@ -23,26 +22,25 @@ func set_available(new_value: bool) -> void:
 	available = new_value
 
 	if available:
+		sprite_2d.modulate = Color.WHITE
 		animation_player.play("highlight")
 	elif not room.selected:
+		sprite_2d.modulate = Color(0.35, 0.35, 0.35)
 		animation_player.play("RESET")
 
 func set_room(new_data: Room) -> void:
 	room= new_data
 	position = room.position
-	line_2d.rotation_degrees = randi_range(0,360)
 	sprite_2d.texture = ICONS[room.type][0]
 	sprite_2d.scale = ICONS[room.type][1]
 
-func show_selected() -> void:
-	line_2d.modulate = Color.WHITE
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if not available or not event.is_action_pressed("left_mouse"):
 		return
 	room.selected = true
 	selected.emit(room) ###########
-	animation_player.play("select")
+	animation_player.play("RESET") 
 
 func _on_map_room_selected() -> void:
 	selected.emit(room)
