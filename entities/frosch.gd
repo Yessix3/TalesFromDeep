3d3d3f52faf57db7fd5ed6fd47f1f8c2dab6c03f
Yessi <yessica.bazatin@gmail.com>
2026-02-24 17:50:46 +0100

    kleine Änderungen Frosch

--- entities/frosch.gd
+++ entities/frosch.gd
@@ -1,46 +1,60 @@
 extends CharacterBody2D
 
-@onready var anim = $AnimationPlayer
-@onready var detection_area: Area2D = $Detection_Area
+var active := false
+signal knockback
 
-var is_jumping = false
-var player_in_range := false
-
-func _ready():
-	anim.animation_started.connect(_on_animation_player_animation_started)
-	anim.animation_finished.connect(_on_animation_player_animation_finished)
-	detection_area.body_entered.connect(_on_detection_entered)
-	detection_area.body_exited.connect(_on_detection_exited)
-	
-	
-func _process(delta:):
-	if player_in_range == false:
-		anim.play("jump")
-	else:
-		anim.stop()
-
-	
-func _on_detection_entered(body: Node):
-	if body.is_in_group("player"):
-		player_in_range = true
+@export var gravity := 600
+@onready var player = get_tree().get_first_node_in_group('Player')
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var attacking := false
 
 
-func _on_detection_exited(body: Node):
-	if body.is_in_group("player"):
-		player_in_range = false
+func _process(delta):
+	if active and not attacking:
+		attacking = true
+		attackPlayer()
+	apply_gravity(delta)
+	move_and_slide()
 
-func _on_animation_player_animation_started(anim_name):
-	pass
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "jump":
-		var offset = $Sprite2D.position.x
-		global_position.x += offset/5
-		$Sprite2D.position.x = 0
-		is_jumping = false
 
-func try_jump():
-	if is_jumping:
-		return
-		
-	is_jumping = true
-	$AnimationPlayer.play("jump")
+func apply_gravity(delta):
+	velocity.y += gravity*delta
+
+
+func attackPlayer():
+	animated_sprite.play("attack")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "recovery": 
+		animated_sprite.play("idle")
+		attacking = false 
+	if animated_sprite.animation == "attack":
+		animated_sprite.play("recovery")
+		$AttackArea/AttackLeft.disabled = true
+		$AttackArea/AttackRight.disabled = true
+	if animated_sprite.animation == "jump":
+		animated_sprite.play("attack")
+		$AttackArea/AttackLeft.disabled = false
+		$AttackArea/AttackRight.disabled = false
+
+
+func _on_attack_area_body_entered(_body: Node2D):
+	emit_signal("knockback")
+
+
+func _on_vision_area_body_entered(_body: Node2D):
+	active = true
+
+
+func _on_vision_area_body_exited(_body: Node2D):
+	active = false
+
+
+func _on_player_player_hit() -> void:
+	if not $Timers/GetHitDelay.time_left :
+		if animated_sprite.animation == "recovery": 
+			velocity.y = -150
+		else:
+			velocity.y = -50
+		$Timers/GetHitDelay.start()
